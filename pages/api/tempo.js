065eda35c7b9ce3b37d7528119b29657c8464e08
Jonathan Felipe <fjajonathan@gmail.com>
2021-05-28 23:22:42 -0300

    teste link 2

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 function tempo (request, response){
 
 const dynamicDate = new Date(); //Guardar tempo atual
 response.json({
     date: dynamicDate.toGMTString()
+     
 });
+
 }
 
 export default tempo;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import link from 'next/link';
+import Link from 'next/link';
 
 function Home() {
     return (<div>
         <h1>Home</h1>
-        <link href= "/api/tempo">
+
+        <Link href="/api/tempo">
             <a>Acessar Página API</a>
-        </link>
-        </div>)
+        </Link>
+    </div>)
 }
 
 export default Home
