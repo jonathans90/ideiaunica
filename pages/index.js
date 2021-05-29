e7dac8e26cc38f4b6b6ec714f8bdb7fd1d04ca4f
Jonathan Felipe <fjajonathan@gmail.com>
2021-05-28 22:56:29 -0300

    Teste de Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import link from 'next/link';
+
 function Home() {
-    return <h2>Home</h2>
+    return (<div>
+        <h1>Home</h1>
+        <link href= "/api/tempo">
+            <a>Acessar Página API</a>
+        </link>
+        </div>)
 }
 
 export default Home
